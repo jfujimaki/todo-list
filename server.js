3e2241fd785da2e82ee067d6c7ea4b714df5366c
julio@upplify.com <jfujimaki@zenbanx.com>
2016-01-24 23:29:53 -0500

    Add search todos by query params

--- server.js
+++ server.js
@@ -26,8 +26,16 @@ app.get("/", function(req, res){
 });
 
 app.get("/todos", function(req, res){
-    //res.send(JSON.stringify(todos));
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if(queryParams.hasOwnProperty("completed") && queryParams.completed == "true"){
+        filteredTodos = _.where(filteredTodos,{completed : true});
+    }else if(queryParams.hasOwnProperty("completed") && queryParams.completed == "false"){
+        filteredTodos = _.where(filteredTodos, {completed : false});
+    }
+
+    res.json(filteredTodos);
 });
 
 app.get("/todos/:id", function(req, res){
